76f601922f1982badbeeaa1306b5f1c431c4522f
Kyle J Turpin <kyle@moturpin.com>
2023-01-31 19:00:58 -0600

    Added test project

---
+++ LogTester/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using static KTLog.KtLog;
+
+void pop()
+{
+    LogStart();
+    Log("Log Proof");
+    try
+    {
+        var zero = 0;
+        var beans = 3 / zero;
+    }
+    catch (Exception ex)
+    {
+        LogWarning(ex);
+    }
+    SignPost();
+    LogEnd();
+    Console.ReadLine();
+}
+
+pop();
